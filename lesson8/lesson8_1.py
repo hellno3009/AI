3f9e7ef02e18f218ba175b04ee30fed757fb026a
hellno3009 <154566379+hellno3009@users.noreply.github.com>
2024-08-21 12:22:50 +0000

    31

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -1,11 +1,11 @@
-import tools
+import widght#或是用from widght import tools直接抓檔案
 while True:
     try:
         name = input("請輸入姓名: ")
         height = float(input('請輸入身高（cm）: '))
         weight = float(input('請輸入體重（kg）: '))
         bmi = weight / ((height * 0.01) ** 2)
-        grade = tools.get_status_message(bmi)
+        grade = widght.tools.get_status_message(bmi)
         print(f"{name} 的 BMI 為 {bmi:.2f}, 為 {grade}")
 
     except ValueError:
