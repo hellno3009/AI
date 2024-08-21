ffbc3a973e716a793a33b1feecf830e7e9cc2182
hellno3009 <154566379+hellno3009@users.noreply.github.com>
2024-08-21 11:46:43 +0000

    30

---
+++ lesson8/lesson8_1.py
@@ -0,0 +1,20 @@
+import tools
+while True:
+    try:
+        name = input("請輸入姓名: ")
+        height = float(input('請輸入身高（cm）: '))
+        weight = float(input('請輸入體重（kg）: '))
+        bmi = weight / ((height * 0.01) ** 2)
+        grade = tools.get_status_message(bmi)
+        print(f"{name} 的 BMI 為 {bmi:.2f}, 為 {grade}")
+
+    except ValueError:
+        print("格式錯誤")
+        continue
+
+    stuff = input("是否繼續輸入資料?(q: 離開,其他鍵:繼續)")
+
+    if stuff == 'q':
+        break
+
+print("程式結束")
